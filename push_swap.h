1c380c3c374a165c6c611217495857ccfd1ae6ba
redgtxt <hp052635@gmail.com>
2024-11-20 12:05:40 +0000

    test

--- push_swap.h
+++ push_swap.h
@@ -1,8 +1,8 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft/libft.h"
-# include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
 
 typedef struct s_node
 {
@@ -13,54 +13,6 @@ typedef struct s_node
 	int chunk;
 }					t_node;
 
-//Debug
-void	print_stack(t_node *stack);
-void	print_stack_ranks(t_node *stack);
-void	print_stack_chunks(t_node *stack);
-
-//Input arguments
-void				write_error(void);
-int					is_digit_str(char *str);
-int					ft_safe_atoi(char *str);
-void				check_duplicate(t_node *head, int num);
-void				argv_convert_builder(int argc, char *argv[], t_node **head);
-
-//Stack Related
-void				add_to_list(t_node **head, int content, int index);
-t_node				*ft_newnode(int content, int index);
-t_node	*create_stackA(int argc, char *argv[]);
-t_node	*create_empty_stack(void);
-void	move_stack(t_node **src, t_node **dest);
-void	copy_stack(t_node *src, t_node **dest);
-void	deletlist(t_node *node);
-int	stacklen(t_node *head);
-int is_sorted(t_node *stackA);
-int find_min_rank(t_node *stackA);
-
-// Funcoes buble sort && Rank
-void				swap_nodes(t_node *a, t_node *b);
-void				assign_ranks(t_node *head);
-void				sort_list(t_node *head);
-void	assign_ranks_to_stackA(t_node *stackA, t_node *stackSort);
-
-
-//Algoritm Chunck sorting
-int calculate_chunks(int num_elements);
-void assign_chunks(t_node *head);
-void	move_all_but_smallest_chunk(t_node **stackA, t_node **stackB);
-void sort_with_moves(t_node **stackA);
-//void sort_single_chunk(t_node **stackA);
-//void   move_current_chunk_to_stackB(t_node **stackA, t_node **stackB, int current_chunk);
-
-// moves
-void				sa(t_node **head);
-void				sb(t_node **head);
-void	pa(t_node **stack_a, t_node **stack_b);
-void	pb(t_node **stack_a, t_node **stack_b);
-void	ra(t_node **head);
-void	rb(t_node **head);
-void	rra(t_node **head);
-void	rrb(t_node **head);
 
 
 #endif
